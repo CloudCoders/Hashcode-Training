7c4e89124168262eab1ce95d4597c93f8b23eb0a
Adriano Vega <adrianovega@hotmail.com>
2017-02-10 19:58:26 +0100

    Small refactor... Still doesn't work lol

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,25 +4,13 @@ import model.Cell
 
 class Main {
 
-    fun main(args: Array<String>) {
-        //R=3, C=4, L = 2, H=4
-        algo(
-                Pizza(
-                        Array<Array<Cell>>(
-
-                        )
-
-        ))
-        println("Hello World!")
-    }
-
-
-
     fun algo(pizza: Pizza): Array<Slice> {
         var i : Int = 0
         var j : Int = 0
         var rowEnd : Int = 0
         var colEnd : Int = 0
+
+
         while(i < pizza.R) {
             if(!pizza.table[i][j].used) {
                 var minIngredients : Boolean = false
@@ -36,23 +24,27 @@ class Main {
                     slice = pizza.table
                             .slice(IntRange(i, rowEnd + 1))
                             .slice(IntRange(j, colEnd))
-                    minIngredients = checkMinElements(slice, pizza.L)
-                    maxSliceSize = checkMaxElements(rowEnd + 1 - i, colEnd - j, pizza.H)
+                    minIngredients = checkMinIngredients(slice, pizza.L)
+                    maxSliceSize = checkMaxSliceSize(rowEnd + 1 - i, colEnd - j, pizza.H)
                     if (minIngredients && maxSliceSize) break
 
                     //2nd Check -> 1 right
                     slice = pizza.table
                             .slice(IntRange(i, rowEnd))
                             .slice(IntRange(j, colEnd+1))
-                    minIngredients = checkMinElements(slice, pizza.L)
-                    maxSliceSize = checkMaxElements(rowEnd + 1 - i, colEnd - j, pizza.H)
-                    if (minIngredients) break
+                    minIngredients = checkMinIngredients(slice, pizza.L)
+                    maxSliceSize = checkMaxSliceSize(rowEnd -i , colEnd + 1 - j, pizza.H)
+                    if (minIngredients && maxSliceSize) break
+
+
                     //3rd Check -> 1 down + 1 right
                     slice = pizza.table
-                            .slice(IntRange(i, rowEnd))
+                            .slice(IntRange(i, rowEnd+1))
                             .slice(IntRange(j, colEnd+1))
-                    minIngredients = checkMinElements(slice, pizza.L)
-                    if (minIngredients) break
+                    minIngredients = checkMinIngredients(slice, pizza.L)
+                    maxSliceSize = checkMaxSliceSize(rowEnd + 1 - i , colEnd + 1 - j, pizza.H)
+                    if (minIngredients && maxSliceSize) break
+
                 }
             }
             i++
@@ -61,7 +53,7 @@ class Main {
 
     }
 
-    fun checkMinElements(slice: List<Array<Cell>>, L: Int): Boolean {
+    fun checkMinIngredients(slice: List<Array<Cell>>, L: Int): Boolean {
         var mushroom: Int = 0
         var tomato: Int = 0
         for(row in slice) {
@@ -72,7 +64,7 @@ class Main {
         return (tomato > L) && (mushroom > L)
     }
 
-    fun checkMaxElements(rows : Int, columns: Int, max : Int) : Boolean {
+    fun checkMaxSliceSize(rows : Int, columns: Int, max : Int) : Boolean {
         return rows*columns < max
     }
 }
